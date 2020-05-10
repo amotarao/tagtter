af44767c1b580914aff9a2affb9c991c4fef9bc7
Amon Sawamura <amon.sawamura@gmail.com>
2020-05-10 15:12:19 +0900

    feat(app): add login button

---
+++ packages/tagtter-app/src/components/pages/TopPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-btn class="text-capitalize mx-auto" tile outlined color="#1da1f2" @click="login">
+      <v-icon left>mdi-twitter</v-icon>Twitterアカウントでログインする
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions({
+      login: 'auth/login',
+    }),
+  },
+};
+</script>
